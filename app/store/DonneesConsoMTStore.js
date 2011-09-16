a92d2de5efe6c4ae67fdb8ed065a2cccc9573117
Tamias <tamias@indiAir.(none)>
2011-09-16 11:55:18 +0200

    tms untracked files

---
+++ app/store/DonneesConsoMTStore.js
@@ -0,0 +1,22 @@
+Ext.define('MainApp.store.DonneesConsoMTStore', {
+    extend: 'Ext.data.Store',
+    requires: 'MainApp.model.DonneesConsoMTModel',
+    model: 'MainApp.model.DonneesConsoMTModel',
+    proxy: {
+    	type: 'ajax',
+    	api: {
+    		read: BASE_URL+'data/facture/loaddonneesconso',
+    	},
+    	actionMethods : {read: 'POST'},   	
+    	reader: {
+    		type: 'json',
+    		root: 'data',
+    		successProperty: 'success'
+    	}
+    },
+    baseParams: {
+        idPl:'' 
+    }
+});
+
+
---
+++ app/store/FactureMTStore.js
@@ -0,0 +1,21 @@
+Ext.define('MainApp.store.FactureMTStore', {
+    extend: 'Ext.data.Store',
+    requires: 'MainApp.model.FactureMTModel',
+    model: 'MainApp.model.FactureMTModel',
+    
+    proxy: {
+    	type: 'ajax',
+    	api: {
+    		read: BASE_URL+'data/facture/load',
+    	},
+    	actionMethods : {read: 'POST'},   	
+    	reader: {
+    		type: 'json',
+    		root: 'data',
+    		successProperty: 'success'
+    	}
+    },
+    baseParams: {
+        idPl:'' 
+    }
+});
